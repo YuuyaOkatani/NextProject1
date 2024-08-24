3f353aab029d76d225a57b0e0b94edd1697d21c9
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-24 19:57:03 -0300

    cookies in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,16 +1,21 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { type NextRequest } from "next/server";
 
 export async function GET(req: NextRequest){
     const requestHeaders = new Headers(req.headers); 
     const headerList = headers()
+    cookies().set("resultsPerPage", "20")
+    const theme = req.cookies.get("theme")
     console.log(requestHeaders.get("Authorization"));
     console.log(headerList.get("Authorization"));
+    console.log(theme)
+    console.log(cookies().get("resultsPerPage"));
     
     
     return new Response("<h2>Profile API data</h2>", {
         headers: {
             "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         },
     });
 }
