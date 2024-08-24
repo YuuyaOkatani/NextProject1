559b9598849c0318c2e4672b8bf0992ce99114ed
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-24 16:54:50 -0300

    Dynamic route handlers

--- src/app/comments/data.ts
+++ src/app/comments/data.ts
@@ -2,16 +2,16 @@ export const comments = [
     {
         id: 1,
         text: "First comment",
-        author: "John Doe"
+      
     },
     {
         id: 2,
         text: "Second comment",
-        author: "Jane Doe"
+    
     },
     {
         id: 3,
         text: "Third comment",
-        author: "Mike Doe"
+        
     }
 ]
