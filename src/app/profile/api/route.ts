42bf0c11c9f0530ad65dd3f272eccb6622ad525b
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-24 19:47:55 -0300

    Headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET(){
-    return new Response("Profile API data");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(req: NextRequest){
+    const requestHeaders = new Headers(req.headers); 
+    const headerList = headers()
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+    
+    
+    return new Response("<h2>Profile API data</h2>", {
+        headers: {
+            "Content-Type": "text/html",
+        },
+    });
 }
